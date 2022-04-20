77bbb7d07d63fe2b3c559ae1192d9f435ab5ca54
Arshaan husain <arshaanhusain16@gmail.com>
2022-04-20 16:24:09 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import {data,MyComponent} from "./MyComponent";
+import { data, MyComponent } from "./MyComponent";
 import { useState } from "react";
 
-
-export default function App(){
-  
-  const [list,setList]=useState(data);
-  const [name,setName]=useState('');
-  function submitHandler(e){
+export default function App() {
+  const [list, setList] = useState(data);
+  const [myAge, setAge] = useState(0);
+  const [name, setName] = useState("");
+  function submitHandler(e) {
     e.preventDefault();
-    setList([...list,
-      {id:Date.now() + Math.random(),title:name}])
+    setList([
+      ...list,
+      { id: Date.now() + Math.random(), title: name, age: myAge },
+    ]);
   }
   console.log(list);
   return (
     <>
-    <form onSubmit={(e)=>submitHandler(e)}>
-    <input value={name}  id="name" onChange={(e)=>setName(e.target.value)}></input>
-      <label htmlFor="name" ></label>
-   <button>Submit</button>
-    </form>
-    <MyComponent list={list}/>
+      <form onSubmit={(e) => submitHandler(e)}>
+        <input
+          value={name}
+          type="text"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <label htmlFor="name"></label>
+        <button>Submit</button>
+      </form>
+      <MyComponent list={list} />
     </>
-  )
+  );
 }
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,32 +1,29 @@
-import DataTable from 'react-data-table-component';
+import DataTable from "react-data-table-component";
 
 const columns = [
-    {
-        name: 'Title',
-        selector: row => row.title,
-    },
-    {
-        name: 'Year',
-        selector: row => row.year,
-    },
+  {
+    name: "Title",
+    selector: (row) => row.title,
+  },
+  {
+    name: "Year",
+    selector: (row) => row.year,
+  },
 ];
 
 export const data = [
-    {
-        id: 1,
-        title: 'Beetlejuice',   
-    },
-    {
-        id: 2,
-        title: 'Ghostbusters',
-    },
-]
+  {
+    id: 1,
+    title: "Beetlejuice",
+    age: 18,
+  },
+  {
+    id: 2,
+    title: "Ghostbusters",
+    age: 20,
+  },
+];
 
-export function MyComponent({list}) {
-    return (
-        <DataTable
-            columns={columns}
-            data={list}
-        />
-    );
-};
+export function MyComponent({ list }) {
+  return <DataTable columns={columns} data={list} />;
+}
